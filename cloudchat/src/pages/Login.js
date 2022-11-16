01e87dded52e7f58cfdfef0580a3b2a271600d68
farmology <89947667+farmology@users.noreply.github.com>
2022-11-15 23:54:46 -0800

    Create email and password states for login

--- cloudchat/src/pages/Login.js
+++ cloudchat/src/pages/Login.js
@@ -1,18 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Col, Container, Row, Form } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import './Login.css'
 
 function Login() {
-  return (
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleLogin(e) {
+        e.preventDefault();
+        //login logic
+    }
+  
+    return (
     <Container>
     <Row>
         <Col md={5} className='login__bg' ></Col>
         <Col md={7} className='d-flex align-items-center justify-content-center flex-direction-column'>
-    <Form style={{ width: '80%', maxWidth: 500 }} >
+    <Form style={{ width: '80%', maxWidth: 500 }} onSubmit={handleLogin}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control type="email" placeholder="Enter email" onChange={(e) => setEmail(e.target.value)} value={email} required/>
         <Form.Text className="text-muted">
           We'll never share your email with anyone else.
         </Form.Text>
@@ -20,7 +28,7 @@ function Login() {
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} value={password} required/>
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicCheckbox">
         <Form.Check type="checkbox" label="Check me out" />
