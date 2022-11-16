ce778d485da5212497c8bb87517b9c77d7e6ef97
farmology <89947667+farmology@users.noreply.github.com>
2022-11-15 23:59:26 -0800

    Create sidebar & message form components for chat

--- cloudchat/src/pages/Chat.js
+++ cloudchat/src/pages/Chat.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+import MessageForm from '../components/MessageForm'
+import Sidebar from '../components/Sidebar'
 
 function Chat() {
   return (
-    <div>Chat</div>
+    <Container>
+      <Row>
+        <Col md={4}>
+          <Sidebar />
+        </Col>
+        <Col md={8} >
+          <MessageForm />
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
