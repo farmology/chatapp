872223b1c52d22f6518666a878007dfb8a9cf6f0
farmology <89947667+farmology@users.noreply.github.com>
2022-11-16 00:25:50 -0800

    Construct chat message box and form

--- cloudchat/src/components/MessageForm.js
+++ cloudchat/src/components/MessageForm.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import { Button, Col, Row, Form } from 'react-bootstrap';
+
 
 function MessageForm() {
-  return (
-    <div>MessageForm</div>
+    function handleSubmit(e) {
+        e.preventDefault();
+    }
+  
+    return (
+    <>
+    <div className='messages-output'></div>
+        <Form onSubmit={handleSubmit}>
+            <Row>
+                <Col md={11}>
+                    <Form.Group>
+                        <Form.Control type='text' placeholder='Type message here' ></Form.Control>
+                    </Form.Group>
+                </Col>
+                <Col md={1}>
+                    <Button variant='primary' type='submit' style={{ width: '100%', backgroundColor: 'blue' }} >
+                        <i className='fas fa-paper-plane' ></i>
+                    </Button>
+                </Col>
+            </Row>
+        </Form>
+    </>
   )
 }
 
