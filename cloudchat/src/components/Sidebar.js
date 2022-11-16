32d6078e6623c48e991a8352d9b759783c9cc234
farmology <89947667+farmology@users.noreply.github.com>
2022-11-16 00:14:16 -0800

    List rooms using map on sidebar

--- cloudchat/src/components/Sidebar.js
+++ cloudchat/src/components/Sidebar.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { ListGroup } from 'react-bootstrap'
 
 function Sidebar() {
+    const rooms = ['general', 'food', 'books', 'travel']
+
   return (
-    <div>Sidebar</div>
+    <>
+        <h2>Rooms</h2>
+        <ListGroup>
+            {rooms.map((room, i) => (
+                <ListGroup.Item key={i}>{room}</ListGroup.Item>
+            ))}
+        </ListGroup>
+        <h2>Members</h2>
+    </>
   )
 }
 
