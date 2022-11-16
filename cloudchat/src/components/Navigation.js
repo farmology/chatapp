434bd1074d76b06507336c95f89e42571f7bcfaf
farmology <89947667+farmology@users.noreply.github.com>
2022-11-15 17:35:52 -0800

    Link logo to home page

--- cloudchat/src/components/Navigation.js
+++ cloudchat/src/components/Navigation.js
@@ -11,7 +11,9 @@ function Navigation() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <LinkContainer to='/'>
+            <Navbar.Brand>CloudChat</Navbar.Brand>
+        </LinkContainer>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
