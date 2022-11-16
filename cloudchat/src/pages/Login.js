3be3a86dc4558565c5e5af05ea783e57e6bd9a42
farmology <89947667+farmology@users.noreply.github.com>
2022-11-15 18:08:20 -0800

    Import and style bootstrap form to login page

--- cloudchat/src/pages/Login.js
+++ cloudchat/src/pages/Login.js
@@ -1,8 +1,36 @@
 import React from 'react'
+import { Button, Col, Container, Row, Form } from 'react-bootstrap'
+import './Login.css'
 
 function Login() {
   return (
-    <div>Login</div>
+    <Container>
+    <Row>
+        <Col md={5} className='login__bg' ></Col>
+        <Col md={7} className='d-flex align-items-center justify-content-center flex-direction-column'>
+    <Form style={{ width: '80%', maxWidth: 500 }} >
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+    </Col>
+    </Row>
+    </Container>
   )
 }
 
