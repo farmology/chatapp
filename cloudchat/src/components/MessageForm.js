74560b6629a840989ee554e2ea21b95f585fd184
farmology <89947667+farmology@users.noreply.github.com>
2022-11-17 16:57:26 -0800

    Disable chat functions if user is not logged in

--- cloudchat/src/components/MessageForm.js
+++ cloudchat/src/components/MessageForm.js
@@ -1,24 +1,29 @@
 import React from 'react'
 import { Button, Col, Row, Form } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
 import './MessageForm.css';
 
 function MessageForm() {
     function handleSubmit(e) {
         e.preventDefault();
     }
-  
+    
+    const user = useSelector((state) => state.user);
+
     return (
     <>
-    <div className='messages-output'></div>
+    <div className='messages-output'>
+        {!user && <div className="alert alert-danger">Please log in.</div>}
+    </div>
         <Form onSubmit={handleSubmit}>
             <Row>
                 <Col md={11}>
                     <Form.Group>
-                        <Form.Control type='text' placeholder='Type message here' ></Form.Control>
+                        <Form.Control type='text' placeholder='Type message here' disabled={!user}></Form.Control>
                     </Form.Group>
                 </Col>
                 <Col md={1}>
-                    <Button variant='primary' type='submit' style={{ width: '100%', backgroundColor: 'blue' }} >
+                    <Button variant='primary' type='submit' style={{ width: '100%', backgroundColor: 'blue' }} disabled={!user}>
                         <i className='fas fa-paper-plane' ></i>
                     </Button>
                 </Col>
--- cloudchat/src/components/Sidebar.js
+++ cloudchat/src/components/Sidebar.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { ListGroup } from 'react-bootstrap'
+import { useSelector } from 'react-redux';
 
 function Sidebar() {
     const rooms = ['general', 'food', 'books', 'travel']
-
+    const user = useSelector((state) => state.user);
+    if (!user) {
+      return <></>;
+    }
   return (
     <>
-        <h2>Rooms</h2>
+        <h2>Topics</h2>
         <ListGroup>
             {rooms.map((room, i) => (
                 <ListGroup.Item key={i}>{room}</ListGroup.Item>
