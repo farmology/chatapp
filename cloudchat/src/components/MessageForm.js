0cb8f3dae8922ace35d678ce630091bd5df0ab74
farmology <89947667+farmology@users.noreply.github.com>
2022-11-17 21:04:01 -0800

    Add function to format date on message form

--- cloudchat/src/components/MessageForm.js
+++ cloudchat/src/components/MessageForm.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Col, Row, Form } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import './MessageForm.css';
 
 function MessageForm() {
+    const [message, setMessage] = useState('');
+
+    function getFormattedDate() {
+        const date = new Date();
+        const year = date.getFullYear();
+        const month = (1 + date.getMonth()).toString();
+        month = month.length > 1 ? month : '0' + month;
+        const day = date.getDate().toString();
+        day = day.length > 1 ? day : '0' + day;
+        return month + '/' + day + '/' + year;
+    }
+
     function handleSubmit(e) {
         e.preventDefault();
     }
