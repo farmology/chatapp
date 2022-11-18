619b8929da83fb84d3821bb008e99cb3e36e1703
farmology <89947667+farmology@users.noreply.github.com>
2022-11-17 19:01:35 -0800

    Display member list on log in
    Member data needs to be saved through refresh

--- cloudchat/src/components/Sidebar.js
+++ cloudchat/src/components/Sidebar.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { ListGroup } from 'react-bootstrap'
 import { useSelector } from 'react-redux';
+import { AppContext } from '../context/appContext';
 
 function Sidebar() {
-    const rooms = ['general', 'food', 'books', 'travel']
+    // const rooms = ['general', 'food', 'books', 'travel']
     const user = useSelector((state) => state.user);
+    const {socket, currentRoom, setCurrentRoom, members, setMembers, messages, setMessages, privateMemberMsg, setPrivateMemberMsg, rooms, setRooms, newMessages, setNewMessages} = useContext(AppContext);
+    socket.off('new-user').on('new-user', (payload) => {
+      console.log(payload);
+      setMembers(payload);
+    } )
     if (!user) {
       return <></>;
     }
+    
+    function getRooms() {
+      fetch('http://localhost:5001/rooms')
+        .then((res) => res.json())
+        .then((data) => setRooms(data));
+    }
+
   return (
     <>
         <h2>Topics</h2>
@@ -17,6 +30,11 @@ function Sidebar() {
             ))}
         </ListGroup>
         <h2>Members</h2>
+        <ListGroup>
+        {members.map((member) => (
+          <ListGroup.Item key={member._id} style={{cursor: 'pointer'}} >{member.name}</ListGroup.Item>
+        ))}
+        </ListGroup>
     </>
   )
 }
