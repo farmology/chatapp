251ed3ca71d19e990148fa4b683dd3472cf48828
farmology <89947667+farmology@users.noreply.github.com>
2022-11-18 18:14:05 -0800

    Add header to rooms and direct messages

--- cloudchat/src/components/MessageForm.js
+++ cloudchat/src/components/MessageForm.js
@@ -49,6 +49,17 @@ function MessageForm() {
     return (
     <>
     <div className='messages-output'>
+            {user && !privateMemberMsg?._id && <div className="alert alert-info">You are in the {currentRoom} room</div>}
+                {user && privateMemberMsg?._id && (
+                    <>
+                        <div className="alert alert-info conversation-info">
+                            <div>
+                                Your conversation with {privateMemberMsg.name} <img src={privateMemberMsg.picture} className="conversation-profile-pic" />
+                            </div>
+                        </div>
+                    </>
+                )}
+        
         {!user && <div className="alert alert-danger">Please log in.</div>}
         
         
