9b48cf57fc00b2d2ce83fa48f680ba18397f0349
farmology <89947667+farmology@users.noreply.github.com>
2022-11-18 18:03:52 -0800

    Display messages grouped by sender and date

--- cloudchat/src/components/MessageForm.js
+++ cloudchat/src/components/MessageForm.js
@@ -45,13 +45,30 @@ function MessageForm() {
     <div className='messages-output'>
         {!user && <div className="alert alert-danger">Please log in.</div>}
         
-        <ListGroup>            
+        {/* <ListGroup>            
             {messages.map((dateArray) => dateArray.messagesByDate.map((msg) => (
                 <ListGroup.Item key={msg._id}>{msg.content}</ListGroup.Item>
             )))        
             }
-        </ListGroup>
-        
+        </ListGroup> */}
+        {user &&
+                    messages.map(({ _id: date, messagesByDate }, idx) => (
+                        <div key={idx}>
+                            <p className="alert alert-info text-center message-date-indicator">{date}</p>
+                            {messagesByDate?.map(({ content, time, from: sender }, msgIdx) => (
+                                <div className={sender?.email == user?.email ? "message" : "incoming-message"} key={msgIdx}>
+                                    <div className="message-inner">
+                                        <div className="d-flex align-items-center mb-3">
+                                            <img src={sender.picture} style={{ width: 35, height: 35, objectFit: "cover", borderRadius: "50%", marginRight: 10 }} />
+                                            <p className="message-sender">{sender._id == user?._id ? "You" : sender.name}</p>
+                                        </div>
+                                        <p className="message-content">{content}</p>
+                                        <p className="message-timestamp-left">{time}</p>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    ))}
     </div>
         <Form onSubmit={handleSubmit}>
             <Row>
